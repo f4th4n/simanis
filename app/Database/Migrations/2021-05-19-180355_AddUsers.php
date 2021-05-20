09f5df51ce2321b1951fbb0a85be560502700fcf
fathan <wilfathan@gmail.com>
2021-05-20 21:48:23 +0700

    Init CI 4

---
+++ app/Database/Migrations/2021-05-19-180355_AddUsers.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddUsers extends Migration {
+	public function up() {
+		$this->forge->addField([
+			'id' => [
+				'type' => 'INT',
+				'constraint' => 5,
+				'unsigned' => true,
+				'auto_increment' => true,
+			],
+			'nama' => [
+				'type' => 'VARCHAR',
+				'constraint' => 256,
+				'unique' => true,
+			],
+			'password' => [
+				'type' => 'VARCHAR',
+				'constraint' => 256,
+			],
+			'keterangan' => [
+				'type' => 'VARCHAR',
+				'constraint' => 256,
+			],
+			'role_id' => [
+				'type' => 'ENUM',
+				'constraint' => ['admin', 'pengecek', 'pemimpin'],
+				'default' => 'admin'
+			],
+		]);
+
+		$this->forge->addPrimaryKey('id');
+		$this->forge->createTable('users');
+	}
+
+	public function down() {
+		$this->forge->dropTable('users');
+	}
+}
