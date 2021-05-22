0db0f397a96db472acdf60d39f749e6830c6a44e
fathan <wilfathan@gmail.com>
2021-05-22 13:32:32 +0700

    Add config

---
+++ app/Config/Simanis.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Simanis extends BaseConfig {
+	public $app_name = 'SI MANIS';
+}
--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+		<title><?= config('Simanis')->app_name ?></title>
 
     <!-- Bootstrap -->
     <link href="/lib/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -33,9 +33,11 @@
             <form action="/admin/login" method="post">
               <h1>Login Admin</h1>
               <div>
-                <input type="text" class="form-control" placeholder="Username" required="" />
+    						<label for="username">Username</label>
+                <input name="username" type="text" class="form-control" placeholder="Username" required="" />
               </div>
               <div>
+    						<label for="password">Passsword</label>
                 <input type="password" class="form-control" placeholder="Password" required="" />
               </div>
               <div>
@@ -50,50 +52,14 @@
 
                 <div>
                   <h1></h1>
-									<p><?= date('Y') ?></p>
+									<p><?= date('Y') ?> | <?= config('Simanis')->app_name ?></p>
                 </div>
               </div>
             </form>
           </section>
         </div>
 
-        <div id="register" class="animate form registration_form">
-          <section class="login_content">
-            <form>
-              <h1>Create Account</h1>
-              <div>
-                <input type="text" class="form-control" placeholder="Username" required="" />
-              </div>
-              <div>
-                <input type="email" class="form-control" placeholder="Email" required="" />
-              </div>
-              <div>
-                <input type="password" class="form-control" placeholder="Password" required="" />
-              </div>
-              <div>
-                <a class="btn btn-default submit" href="index.html">Submit</a>
-              </div>
-
-              <div class="clearfix"></div>
-
-              <div class="separator">
-                <p class="change_link">Already a member ?
-                  <a href="#signin" class="to_register"> Log in </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br />
-
-                <div>
-                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
-                </div>
-              </div>
-            </form>
-          </section>
-        </div>
       </div>
     </div>
   </body>
 </html>
-
